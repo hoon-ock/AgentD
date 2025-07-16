97e85986d0700075c42ccc83059895cc25cf80ed
hoon-ock <jock@andrew.cmu.edu>
2025-07-16 11:06:47 -0400

    setup update

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "requests==2.32.3",
         "torch==2.6.0",
     ],
-    python_requires="==3.10.*",
+    python_requires="==3.11.*",
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
